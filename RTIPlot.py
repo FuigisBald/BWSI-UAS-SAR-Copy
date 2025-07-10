392d41b4b1992c0922ab3e89763dfcf8dd0d865e
Diego <diego.abadie.l@outlook.com>
2025-07-10 16:47:41 -0400

    Added Decibel conversion

--- RTIPlot.py
+++ RTIPlot.py
@@ -9,22 +9,26 @@ c = 299,792,458
  
 ranges = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
 
-intensity = np.array([[10, 15, 20, 25, 30, 25, 20, 15, 10, 5],
+amplitude = np.array([[10, 15, 20, 25, 30, 25, 20, 15, 10, 5],
             [12, 17, 22, 27, 32, 27, 22, 17, 12, 7],
             [8, 13, 18, 23, 28, 23, 18, 13, 8, 3],
             [15, 20, 25, 30, 35, 30, 25, 20, 15, 10],
-            [5, 10, 15, 20, 25, 20, 15, 10, 5, 0],
+            [5, 10, 15, 20, 25, 20, 15, 10, 5, 1],
             [20, 25, 30, 35, 40, 35, 30, 25, 20, 15],
             [7, 12, 17, 22, 27, 22, 17, 12, 7, 2],
             [18, 23, 28, 33, 38, 33, 28, 23, 18, 13],
             [3, 8, 13, 18, 23, 18, 13, 8, 3, -2],
             [25, 30, 35, 40, 45, 40, 35, 30, 25, 20]])
+
+
+db = 20 * np.log10(np.abs(amplitude))
+
 arr = []
 #Prints all elements of 3D
 for i in range(10):
     zist=[]
     for j in range(10):
-        Z=intensity[i,j]
+        Z=db[i,j]
         zist.append(Z)
     arr.append(zist)    
 
@@ -36,11 +40,3 @@ plt.xticks(range(len(ranges)), ranges)
 plt.yticks(range(10))
 plt.colorbar()
 plt.show()
-
-"""
-def convertToDecibles():
-
-def findIntensity():
-    point = intensity[0, 0]
-    print(point)
-"""
