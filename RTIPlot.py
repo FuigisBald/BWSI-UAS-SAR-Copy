2faa82f6e241caa95683ae1504b749cd690033ee
Diego <diego.abadie.l@outlook.com>
2025-07-10 16:50:50 -0400

    Added comments and cleaned up code

--- RTIPlot.py
+++ RTIPlot.py
@@ -5,10 +5,7 @@ import numpy as np
 c = 299,792,458
 
 #Sample data from deepseek
-
- 
 ranges = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
-
 amplitude = np.array([[10, 15, 20, 25, 30, 25, 20, 15, 10, 5],
             [12, 17, 22, 27, 32, 27, 22, 17, 12, 7],
             [8, 13, 18, 23, 28, 23, 18, 13, 8, 3],
@@ -20,21 +17,20 @@ amplitude = np.array([[10, 15, 20, 25, 30, 25, 20, 15, 10, 5],
             [3, 8, 13, 18, 23, 18, 13, 8, 3, -2],
             [25, 30, 35, 40, 45, 40, 35, 30, 25, 20]])
 
-
+#Converts the amplitude array into decibels
 db = 20 * np.log10(np.abs(amplitude))
 
+#Converts the test data into a 3d Array
 arr = []
-#Prints all elements of 3D
 for i in range(10):
     zist=[]
     for j in range(10):
         Z=db[i,j]
         zist.append(Z)
     arr.append(zist)    
-
 arr = np.array(arr)
-print(arr)
 
+#Plots the data
 plt.imshow(arr, aspect='equal')
 plt.xticks(range(len(ranges)), ranges)
 plt.yticks(range(10))
