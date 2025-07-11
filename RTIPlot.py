578f426beb68ef15ba94c9c23334128a7dde2f12
Diego <diego.abadie.l@outlook.com>
2025-07-10 23:10:43 -0400

    Simplified code

--- RTIPlot.py
+++ RTIPlot.py
@@ -15,24 +15,28 @@ amplitude = np.array([[10, 15, 20, 25, 30, 25, 20, 15, 10, 5],
             [7, 12, 17, 22, 27, 22, 17, 12, 7, 2],
             [18, 23, 28, 33, 38, 33, 28, 23, 18, 13],
             [3, 8, 13, 18, 23, 18, 13, 8, 3, -2],
-            [25, 30, 35, 40, 45, 40, 35, 30, 25, 20]])
+            [25, 30, 35, 40, 45, 40, 35, 30, 25, 20]]) 
 
 #Converts the amplitude array into decibels
 db = 20 * np.log10(np.abs(amplitude))
 
-#Converts the test data into a 3d Array
+#Converts the test data into a 3d Array -> we don't need this yet. Could be useful for when we get the data and try to convert into what we have above
+"""
 arr = []
-for i in range(10):
+for i in range(len(amplitude)):
     zist=[]
-    for j in range(10):
+    for j in range(len(amplitude[0])):
         Z=db[i,j]
         zist.append(Z)
     arr.append(zist)    
 arr = np.array(arr)
+print (arr)
 
+"""
 #Plots the data
-plt.imshow(arr, aspect='equal')
+plt.imshow(db, aspect='equal')
 plt.xticks(range(len(ranges)), ranges)
 plt.yticks(range(10))
 plt.colorbar()
 plt.show()
+
