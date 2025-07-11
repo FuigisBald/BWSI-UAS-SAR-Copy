222240a5e62eb7e35a6f8415d145ed66de93377c
Diego <diego.abadie.l@outlook.com>
2025-07-11 11:50:11 -0400

    WORKING RTI HYPE
    Still need to convert to range

--- RTIPlot.py
+++ RTIPlot.py
@@ -1,21 +1,56 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+import json
 #Speed of light 
 c = 299,792,458*10^-12
 
+with open('scans-2024-06-26_23-07-29.json', 'r') as f:
+    receivedData = json.load(f)
 #Sample data from deepseek transformed into how we will actually be receiving the data
+receivedAmplitude = []
+longTime = []
+for i in range (len(receivedData)):
+    receivedAmplitude.append(receivedData[i][1])
+    longTime.append(receivedData[i][0])
+4
+
+#Converts the amplitude array into decibels
+db = 20 * np.log10(np.abs(receivedAmplitude))
+#Plots the data
+plt.imshow(db, aspect='auto', extent=(0, len(receivedData[0][1])*61+300, receivedData[0][0], (receivedData[-1][0])))
+#plt.xticks(10, (len(receivedData[0][1]))*61+300)
+#plt.yticks(longTime)
+plt.colorbar()
+plt.show()
+
+
+
 """
-receivedData = ([[0.1,[10, 15, 20, 25, 30, 25, 20, 15, 10, 5]],
-                    [0.2,[12, 17, 22, 27, 32, 27, 22, 17, 12, 7]], 
-                    [0.3,[8, 13, 18, 23, 28, 23, 18, 13, 8, 3]],
-                    [0.4,[15, 20, 25, 30, 35, 30, 25, 20, 15, 10]],
-                    [0.5,[5, 10, 15, 20, 25, 20, 15, 10, 5, 1]],
-                    [0.6,[20, 25, 30, 35, 40, 35, 30, 25, 20, 15]],
-                    [0.7,[7, 12, 17, 22, 27, 22, 17, 12, 7, 2]],
-                    [0.8,[18, 23, 28, 33, 38, 33, 28, 23, 18, 13]],
-                    [0.9,[25, 30, 35, 40, 45, 40, 35, 30, 25, 20]]])
-"""
+#Sample data from deepseek
+ranges = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
+amplitude = np.array([[10, 15, 20, 25, 30, 25, 20, 15, 10, 5],
+            [12, 17, 22, 27, 32, 27, 22, 17, 12, 7],
+            [8, 13, 18, 23, 28, 23, 18, 13, 8, 3],
+            [15, 20, 25, 30, 35, 30, 25, 20, 15, 10],
+            [5, 10, 15, 20, 25, 20, 15, 10, 5, 1],
+            [20, 25, 30, 35, 40, 35, 30, 25, 20, 15],
+            [7, 12, 17, 22, 27, 22, 17, 12, 7, 2],
+            [18, 23, 28, 33, 38, 33, 28, 23, 18, 13],
+            [3, 8, 13, 18, 23, 18, 13, 8, 3, -2],
+            [25, 30, 35, 40, 45, 40, 35, 30, 25, 20]]) 
+
+            #Converts the test data into a 3d Array -> we don't need this yet. Could be useful for when we get the data and try to convert into what we have above
+
+arr = []
+for i in range(len(amplitude)):
+    zist=[]
+    for j in range(len(amplitude[0])):
+        Z=db[j,i]
+        zist.append(Z)
+    arr.append(zist)    
+arr = np.array(arr)
+print (arr)
+
 receivedData = ([[0.1, [10, 15, 20, 25, 30, 25, 20, 15, 10, 5]],
                 [0.2, [12, 17, 22, 27, 32, 27, 22, 17, 12, 7]], 
                 [0.3, [8, 13, 18, 23, 28, 23, 18, 13, 8, 3]],
@@ -116,49 +151,4 @@ receivedData = ([[0.1, [10, 15, 20, 25, 30, 25, 20, 15, 10, 5]],
                 [9.8, [135, 150, 165, 180, 195, 180, 165, 150, 135, 130]],
                 [9.9, [180, 185, 190, 195, 200, 195, 190, 185, 180, 175]],
                 [10.0, [155, 165, 175, 185, 195, 185, 175, 165, 155, 150]]])
-receivedAmplitude = []
-longTime = []
-for i in range (len(receivedData)):
-    receivedAmplitude.append(receivedData[i][1])
-    longTime.append(receivedData[i][0])
-print(longTime)
-
-#len(receivedData[0][1]))*61+300
-
-#Converts the amplitude array into decibels
-db = 20 * np.log10(np.abs(receivedAmplitude))
-#Plots the data
-plt.imshow(db, aspect='auto', extent=(0, len(receivedData[0][1])*61+300, receivedData[0][0], (receivedData[-1][0])))
-#plt.xticks(10, (len(receivedData[0][1]))*61+300)
-#plt.yticks(longTime)
-plt.colorbar()
-plt.show()
-
-
-
-"""
-#Sample data from deepseek
-ranges = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
-amplitude = np.array([[10, 15, 20, 25, 30, 25, 20, 15, 10, 5],
-            [12, 17, 22, 27, 32, 27, 22, 17, 12, 7],
-            [8, 13, 18, 23, 28, 23, 18, 13, 8, 3],
-            [15, 20, 25, 30, 35, 30, 25, 20, 15, 10],
-            [5, 10, 15, 20, 25, 20, 15, 10, 5, 1],
-            [20, 25, 30, 35, 40, 35, 30, 25, 20, 15],
-            [7, 12, 17, 22, 27, 22, 17, 12, 7, 2],
-            [18, 23, 28, 33, 38, 33, 28, 23, 18, 13],
-            [3, 8, 13, 18, 23, 18, 13, 8, 3, -2],
-            [25, 30, 35, 40, 45, 40, 35, 30, 25, 20]]) 
-
-            #Converts the test data into a 3d Array -> we don't need this yet. Could be useful for when we get the data and try to convert into what we have above
-
-arr = []
-for i in range(len(amplitude)):
-    zist=[]
-    for j in range(len(amplitude[0])):
-        Z=db[j,i]
-        zist.append(Z)
-    arr.append(zist)    
-arr = np.array(arr)
-print (arr)
 """
