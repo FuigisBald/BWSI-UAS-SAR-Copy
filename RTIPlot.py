5909a4f0c505ba7a33d380284b892848829c58be
Diego <diego.abadie.l@outlook.com>
2025-07-14 15:04:56 -0400

    Added Pull Range function

--- RTIPlot.py
+++ RTIPlot.py
@@ -3,7 +3,7 @@ import numpy as np
 import json
 
 #Pulls data from json file
-with open('scans-2025-07-11_16-35-23.json', 'r') as f:
+with open('scans-2024-06-26_22-48-47.json', 'r') as f:
     receivedData = json.load(f)
 
 #Sample data from deepseek transformed into how we will actually be receiving the data
@@ -13,6 +13,10 @@ for i in range (len(receivedData)):
     receivedAmplitude.append(receivedData[i][1])
     longTime.append(receivedData[i][0])
 
+def pullRange(index):
+    return index*61*(10e-13)*299792458/2 #still have to add the start range variable here
+
+
 #If we set range scale, we avoid a massive 3d array with a lot of repeating data points.
 #We can still get the same RTI graph by just setting the scale seperately
 rangeScale = ((len(receivedData[0][1])*61+23349)*299792458*(10e-13))/2
@@ -21,6 +25,6 @@ rangeScale = ((len(receivedData[0][1])*61+23349)*299792458*(10e-13))/2
 db = 20 * np.log10(np.abs(receivedAmplitude))
 
 #Plots the data
-plt.imshow(db, aspect='auto', extent=(3.5, rangeScale, receivedData[-1][0], receivedData[0][0]))
+plt.imshow(db, aspect='auto', extent=(3.5, rangeScale, receivedData[-1][0], receivedData[0][0])) 
 plt.colorbar()
 plt.show()
