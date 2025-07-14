20547524d287a4bc835a4fab418d54f8f37e4a3e
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-07-14 10:09:32 -0400

    added amplitude_graph.py to graph individual scans

---
+++ amplitude_graph.py
@@ -0,0 +1,26 @@
+import json
+import matplotlib.pyplot as plt
+
+#Pulls data from json file
+with open('scans-2025-07-11_15-55-48.json', 'r') as f:
+    receivedData = json.load(f)
+
+#Sample data from deepseek transformed into how we will actually be receiving the data
+receivedAmplitude = []
+ranges = []
+
+for i, amplitude in enumerate(receivedData[0][1]):
+    time = i*61 + 23349
+    range = time * 299792458 * (10e-13) / 2  # Convert time to range in meters
+    ranges.append(range)
+    receivedAmplitude.append(amplitude * range**4)
+
+# Plot amplitude vs time
+plt.figure(figsize=(10, 6))
+plt.plot(ranges, receivedAmplitude, label='Amplitude')
+plt.title('Amplitude vs Range')
+plt.xlabel('Range (m)')
+plt.ylabel('Amplitude')
+plt.legend()
+plt.grid()
+plt.show()
