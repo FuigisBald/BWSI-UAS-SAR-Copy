ec6445c252b2e7c49355c2f3d1eb1072fcedfbd1
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-07-14 15:22:53 -0400

    added RTI support for scan start and end in json

--- RTIPlot.py
+++ RTIPlot.py
@@ -2,25 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-#Pulls data from json file
-with open('scans-2025-07-11_16-35-23.json', 'r') as f:
-    receivedData = json.load(f)
+# Pulls data from json file
+with open("scans-2025-07-11_18-42-43.json", "r") as f:
+    received_data = json.load(f)
 
-#Sample data from deepseek transformed into how we will actually be receiving the data
-receivedAmplitude = []
-longTime = []
-for i in range (len(receivedData)):
-    receivedAmplitude.append(receivedData[i][1])
-    longTime.append(receivedData[i][0])
+# Gets the scan start and end times
+scan_start = received_data["scan_start"]
+scan_end = received_data["scan_end"]
 
-#If we set range scale, we avoid a massive 3d array with a lot of repeating data points.
-#We can still get the same RTI graph by just setting the scale seperately
-rangeScale = ((len(receivedData[0][1])*61+23349)*299792458*(10e-13))/2
+scans = []
+long_time = []
+
+for i, scan in enumerate(received_data["scans"]):
+    amplitudes = []
+    for j, amplitude in enumerate(scan[1]):
+        range = (j*61 + scan_start) * 299792458 * (10e-13) / 2  # Convert time (ps) to range (m)
+        amplitudes.append(amplitude)
+    scans.append(amplitudes)
+    long_time.append(scan[0])
+
+range_start = scan_start * 299792458 * (10e-13) / 2
+range_end = scan_end * 299792458 * (10e-13) / 2
 
 #Converts the amplitude array into decibels
-db = 20 * np.log10(np.abs(receivedAmplitude))
+db = 20 * np.log10(np.abs(scans))
 
-#Plots the data
-plt.imshow(db, aspect='auto', extent=(3.5, rangeScale, receivedData[-1][0], receivedData[0][0]))
+# Plots the data
+plt.imshow(
+    db, aspect="auto", extent=(range_start, range_end, long_time[-1], long_time[0]),
+)
 plt.colorbar()
 plt.show()
--- amplitude_graph.py
+++ amplitude_graph.py
@@ -2,15 +2,17 @@ import json
 import matplotlib.pyplot as plt
 
 #Pulls data from json file
-with open('scans-2025-07-11_15-55-48.json', 'r') as f:
+with open('scans-2025-07-11_17-11-48.json', 'r') as f:
     receivedData = json.load(f)
 
 #Sample data from deepseek transformed into how we will actually be receiving the data
 receivedAmplitude = []
 ranges = []
 
-for i, amplitude in enumerate(receivedData[0][1]):
-    time = i*61 + 23349
+scan_start = receivedData["scan_start"]
+
+for i, amplitude in enumerate(receivedData["scans"][0][1]):
+    time = i*61 + scan_start
     range = time * 299792458 * (10e-13) / 2  # Convert time to range in meters
     ranges.append(range)
     receivedAmplitude.append(amplitude * range**4)
