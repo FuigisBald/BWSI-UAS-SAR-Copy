5ef20c889ee1900062aa1bcfb694245c9f3e09b0
BarryTheLadCodes <themaskedboiii2@gmail.com>
2025-07-15 14:01:10 -0400

    changed backprojection to add amplitudes instead of intensities

--- backprojection.py
+++ backprojection.py
@@ -10,11 +10,9 @@ start_time = time.time()
 with open("5_point_scatter.pkl", "rb") as f:
     receivedData = pickle.load(f)
 
-data_set = receivedData.get("scan_data")
+data_set = receivedData.get("scan_data").real
 positions = receivedData.get("platform_pos")
 range_bins = receivedData.get("range_bins")
-
-db_set = 20 * np.log10(np.abs(data_set))
 
 # Single backprojection for first frame
 drone_pos = (
@@ -24,11 +22,10 @@ drone_pos = (
 )  # Drone position for first frame
 grid_resolution = (100, 100)  # In pixels, adjust as needed
 max_ranges = (-50, 50)  # In meters, adjust as needed
-backprojected_intensities = np.empty(grid_resolution)
 r_res = max_ranges[0] / grid_resolution[0]  # Range resolution in meters
 c_res = max_ranges[1] / grid_resolution[1]  # Cross-range resolution
 
-added_intensities = np.zeros(
+added_amplitudes = np.zeros(
     shape=(grid_resolution[0], grid_resolution[1])
 )  # Base array for adding up intensities
 for z in range(len(positions)):
@@ -41,23 +38,25 @@ for z in range(len(positions)):
             )  # Converts pixel coordinates to meters
             # Calculate the distance from the drone to the pixel coordinates
             distance = np.sqrt(
-                (positions[z][0] - pixel_coords_meters[0]) ** 2
-                + (positions[z][1] - pixel_coords_meters[1]) ** 2
-                + positions[z][2] ** 2
+                (positions[z][0] - pixel_coords_meters[0]) ** 2 + # X Axis
+                (positions[z][1] - pixel_coords_meters[1]) ** 2 + # Y Axis
+                positions[z][2] ** 2 # Z Axis
             )
             index = np.argmin(
                 np.abs(range_bins - distance)
             )  # Find index of the closest range bin
-            added_intensities[k][j] += db_set[z][index]  # Adds the amplitude at the pixel coordinates for each frame
+            added_amplitudes[k][j] += data_set[z][index]  # Adds the amplitude at the pixel coordinates for each frame
 
-added_intensities /= 200 # Normalize 
+back_projection_intensities = 20 * np.log10(np.abs(added_amplitudes))  # Convert to dB scale
+
+#back_projection_intensities /= 200 # Normalize 
 plt.imshow(
-    added_intensities,
+    back_projection_intensities,
     extent=(max_ranges[0] / 2, max_ranges[1] / 2, max_ranges[0] / 2, max_ranges[1] / 2),
     cmap="plasma",
     origin="lower",
 )
-plt.colorbar(label="Amplitude (dB)")
+plt.colorbar(label="Intensity (dB)")
 plt.title("Backprojection of Radar Data")
 plt.xlabel("Cross-range (m)")
 plt.ylabel("Range (m)")
