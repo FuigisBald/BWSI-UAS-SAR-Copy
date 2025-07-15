fa6e5a1301d0a3746fa939fceb06e17d2a3544b8
zoyam64 <zoyamoloo.64@gmail.com>
2025-07-15 10:39:26 -0400

    Co-authored-by: sophiehe54 <sophiehe54@users.noreply.github.com> Co-authored-by: FuigisBald <FuigisBald@users.noreply.github.com> Co-authored-by: FlatterBaker603 <FlatterBaker603@users.noreply.github.com>

--- backprojection.py
+++ backprojection.py
@@ -6,62 +6,78 @@ import numpy as np
 with open('5_point_scatter.pkl', 'rb') as f:
     receivedData = pickle.load(f)
 
-amplitudes = receivedData.get('scan_data') #200 lists of 2551 amplitudes
-rangebins = receivedData.get('range_bins') #2551 range bins
-location = receivedData.get('platform_pos') #200 locations
-
-#Converts the amplitude array into decibels
-db = 20 * np.log10(np.abs(amplitudes))
-
-circent = (-20,-15) #hardcoded location of drone
-colormap = plt.colormaps['viridis']
-#max = 90
-#min = -30 this is around what I found for the first frame, could be changed or shifted etc
-fig, ax = plt.subplots()
-
-for i in range(len(rangebins)):
-    amp = db[0][i] #hardcoded 0 for the first frame, change for integration
-    circ = plt.Circle(circent,rangebins[i],fill=False,color=colormap((amp+30)/(120))) #fix hardcoded min and max
-    ax.add_patch(circ)
-
-ax.set_xlim(-50, 50) #find a more reasonable plot size
-ax.set_ylim(-50, 50)
-ax.set_aspect('equal')
-#plt.colorbar()
-plt.show()
-
-""""
-could keep or delete, I just didn't want to delete it while merging...
 data_set = receivedData.get('scan_data')
-position = receivedData.get('platform_pos')
+positions = receivedData.get('platform_pos')
 range_bins = receivedData.get('range_bins')
-print(len(range_bins)) #prints 2551
-db_set= 20 * np.log10(np.abs(data_set))
-print(len(data_set[1])) #prints 2551
-print(data_set[1])
-print("data_set shape:", np.array(data_set).shape)  
-print("range_bins shape:", np.array(range_bins).shape)
-plt.scatter(range_bins, data_set[1], s=1)
-plt.show() #multiple db_set[0] values for every range_bins value
-"""
-""""
-This earlier gave a very clear graph but not sure if relavent to what we gotta do. im just commenting it out for now
 
-plt.imshow(db_set, aspect='auto', extent=[#xstart ystart, range_bins[0], range_bins[-1]])
+db_set = 20 * np.log10(np.abs(data_set))
+
+#Single backprojection for first frame
+drone_pos = (positions[0][0], positions[0][1], positions[0][2]) # Drone position for first frame
+grid_resolution = (100, 100) # In pixels, adjust as needed
+max_ranges = (10, 10) # In meters, adjust as needed
+backprojected_intensities = np.empty(grid_resolution)
+r_res = max_ranges[0] / grid_resolution[0] # Range resolution in meters
+c_res = max_ranges[1] / grid_resolution[1] # Cross-range resolution
+
+# Getting the range bin index for each pixel in the backprojected image
+# for cross_range in range(grid_resolution[0]):
+#     for range_val in range(grid_resolution[1]):
+#         pixel_coords = (cross_range, range_val)
+#         pixel_coords_meters = (pixel_coords[0] * c_res, pixel_coords[1] * r_res)
+#         distance = np.sqrt(
+#             (pixel_coords_meters[0] - drone_pos[0]) ** 2 + # X Axis
+#             (pixel_coords_meters[1] - drone_pos[1]) ** 2 + # Y Axis
+#             (0 - drone_pos[2]) ** 2 # Z Axis (assuming ground level is 0)
+#         )
+    
+#     distance_index = np.argmin(np.abs(range_bins - distance))
+#     intensity = db_set[0][distance_index]
+#     backprojected_intensities[cross_range][range_val] = intensity
+
+# plt.imshow(
+#     backprojected_intensities, extent=[0, max_ranges[0], 0, max_ranges[0]], cmap="inferno"
+# )
+# plt.colorbar(label='Amplitude (dB)')
+# plt.show()
+added_amplitudes =  np.zeros(shape=(grid_resolution[0], grid_resolution[1]))
+for z in range(len(positions)):
+    for j in range(grid_resolution[0]):
+        for k in range(grid_resolution[1]):
+            distance = np.sqrt((positions[z][0]-j)**2 + (positions[z][1]-k)**2 + positions[z][2]**2)
+            for i in range (len(range_bins)):
+                if(range_bins[i] > distance):
+                    if(np.abs(range_bins[i-1] - distance) > np.abs(range_bins[i] - distance)):
+                        index = i-1
+                    else:
+                        index = i
+                    break
+            added_amplitudes[j][k] += data_set[z][index]
+
+plt.imshow(added_amplitudes)
 plt.show()
-"""
 
-"""
+# # RPI Plot
+# plt.imshow(db_set, aspect='auto', extent=[range_bins[0], range_bins[-1], positions[0][0], positions[-1][0]], cmap="inferno")
+# plt.colorbar(label='Amplitude (dB)')
+# plt.xlabel('Range (m)')
+# plt.ylabel('Position (m)')
+# plt.title('RPI')
+# plt.show()
 
-This code is for adding up amplitudes. Probably won't need it and just delete it but im keeping it here just in case
+# Scatter plot of the first frame
+# plt.scatter(range_bins, data_set[1], s=1)
+# plt.show() #multiple db_set[0] values for every range_bins value
 
-backprojected_amplitudes = db_set[0]
-for j in range (len(db_set)-1):
-    for i in range (len(db_set[0])):
-        backprojected_amplitudes[i] += db_set[j+1][i]
+# This code is for adding up amplitudes. Probably won't need it and just delete it but im keeping it here just in case
 
-print(backprojected_amplitudes)
+# backprojected_intensities = db_set[0]
+# for j in range (len(db_set)-1):
+#     for i in range (len(db_set[0])):
+#         backprojected_intensities[i] += db_set[j+1][i]
 
-plt.scatter(backprojected_amplitudes, range_bins, color='red', label="Data Points") 
-plt.show()
-"""
+# print(backprojected_intensities)
+
+# plt.scatter(backprojected_intensities, range_bins, color='red', label="Data Points") 
+# plt.show()
+# """
